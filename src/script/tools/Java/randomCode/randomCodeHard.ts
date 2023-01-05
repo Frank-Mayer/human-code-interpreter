e769c1b5e79c60ec2ba731c7e1eabb874fcb7dd2
Frank-Mayer <mail@frank-mayer.io>
2023-01-05 13:27:21 +0100

    added code sample to hard

--- src/script/tools/Java/randomCode/randomCodeHard.ts
+++ src/script/tools/Java/randomCode/randomCodeHard.ts
@@ -103,10 +103,76 @@ const randomCodeHard_003 = (): JProg => {
     );
 };
 
+const randomCodeHard_004 = (): JProg => {
+    const varNames = getVarNames();
+    const start = rInt(12, 15);
+    const end = start + rInt(6, 7);
+
+    return new JProg(
+        ["import java.util.ArrayList;"],
+        new JClass("Main", [
+            new JMethod(
+                ["public", "static"],
+                "main",
+                "void",
+                [new JParameter("args", "String[]")],
+                [
+                    `for(int i = ${start}; i <= ${end}; i++) {`,
+                    `${tab}switch (i % 15) {`,
+                    `${tab}${tab}case 0:`,
+                    `${tab}${tab}${tab}System.out.println("${varNames[0]}");`,
+                    `${tab}${tab}${tab}break;`,
+                    `${tab}${tab}case 3:`,
+                    `${tab}${tab}case 6:`,
+                    `${tab}${tab}case 9:`,
+                    `${tab}${tab}case 12:`,
+                    `${tab}${tab}${tab}System.out.println("${varNames[1]}");`,
+                    `${tab}${tab}${tab}break;`,
+                    `${tab}${tab}case 5:`,
+                    `${tab}${tab}case 10:`,
+                    `${tab}${tab}${tab}System.out.println("${varNames[2]}");`,
+                    `${tab}${tab}${tab}break;`,
+                    `${tab}${tab}default:`,
+                    `${tab}${tab}${tab}System.out.println(i);`,
+                    `${tab}${tab}${tab}break;`,
+                    `${tab}}`,
+                    `}`,
+                ].join("\n")
+            ),
+        ]),
+        [],
+        () => {
+            const result = new Array<string>();
+            for (let i = start; i <= end; i++) {
+                switch (i % 15) {
+                    case 0:
+                        result.push(varNames[0]);
+                        break;
+                    case 3:
+                    case 6:
+                    case 9:
+                    case 12:
+                        result.push(varNames[1]);
+                        break;
+                    case 5:
+                    case 10:
+                        result.push(varNames[2]);
+                        break;
+                    default:
+                        result.push(i.toString());
+                        break;
+                }
+            }
+            return result.join("\n");
+        }
+    );
+};
+
 export const randomCodeHard = (): JProg => {
     return pickRandom(
         randomCodeHard_001,
         randomCodeHard_002,
-        randomCodeHard_003
+        randomCodeHard_003,
+        randomCodeHard_004
     )();
 };
