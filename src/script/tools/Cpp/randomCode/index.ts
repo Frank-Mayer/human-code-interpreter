3ef6fdce780ceb326f12c42c47932f771b91c061
Frank-Mayer <git@frank-mayer.io>
2023-02-21 15:35:19 +0100

    full c++

--- src/script/tools/Cpp/randomCode/index.ts
+++ src/script/tools/Cpp/randomCode/index.ts
@@ -1,6 +1,6 @@
 import { randomCodeEasy } from "./randomCodeEasy";
-// import { randomCodeMedium } from "./randomCodeMedium";
-// import { randomCodeHard } from "./randomCodeHard";
+import { randomCodeMedium } from "./randomCodeMedium";
+import { randomCodeHard } from "./randomCodeHard";
 import type { VirtualProg } from "../../VirtualProg";
 import { Result } from "@frank-mayer/opsult";
 
@@ -8,10 +8,10 @@ export const randomCode = (difficulty: number): Result<VirtualProg, string> => {
     switch (difficulty) {
         case 1:
             return Result.Ok(randomCodeEasy());
-        // case 2:
-        //     return Result.Ok(randomCodeMedium());
-        // case 3:
-        //     return Result.Ok(randomCodeHard());
+        case 2:
+            return Result.Ok(randomCodeMedium());
+        case 3:
+            return Result.Ok(randomCodeHard());
         default:
             return Result.Err(`Invalid difficulty: ${difficulty}.`);
     }
